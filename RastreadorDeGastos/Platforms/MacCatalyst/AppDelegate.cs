3abfbe57a5cc0cdcb084934a36be36e2ecd29c9d
Giovani <giofdz00@gmail.com>
2024-11-17 20:04:28 -0600

    Se crea proyecto

---
+++ RastreadorDeGastos/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace RastreadorDeGastos
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
